34ee954e54ef823b13aceb1c813eae4891983e53
chintan23aum <chintan@aum.bz>
2024-03-02 09:54:23 +0530

    Category crud option started on admin panel

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -21,16 +23,16 @@ class DashboardController extends AbstractDashboardController
         /** @var User $user */
         $user = $this->getUser();
 
-       /* return match ($user->isVerified()) {
+        /*return match ($user->isVerified()) {
             true => $this->render("admin/index.html.twig"),
             false => $this->render("admin/please-verify-email.html.twig"),
         };*/
         //return parent::index();
-        return $this->render('admin/index.html.twig');
+       // return $this->render('admin/index.html.twig');
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+         return $this->redirect($adminUrlGenerator->setController(CategoryCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -53,6 +55,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+         yield MenuItem::linkToCrud('Category', 'fas fa-list', Category::class);
     }
 }
